c53292384788f704aa28248b84f640bef4c270c3
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-24 12:06:46 +0530

    added dynamic routes and play with json parsing in gofiber

--- main.go
+++ main.go
@@ -9,5 +9,21 @@ func main(){
 	app.Get("/hello",func(c*fiber.Ctx)error{
 		return c.SendString("NAMASKARAM KYA HELLO ?")
 	})
+	app.Get("/hello/:id", func(c*fiber.Ctx)error{
+		id := c.Params("id")
+		return c.SendString("UserId:"+id)
+	})
+
+	type User struct{
+		Name string `Json: "name"`
+		Age int `Json: "age"`
+	}
+	app.Post("/user", func(c*fiber.Ctx)error{
+		user := new(User)
+		if err:= c.BodyParser(user); err != nil{
+			return err
+		}
+		return c.JSON(user)
+	})
 	app.Listen(":3000")
 }
